a13aac92c67db8d02186e7773c42f4f8c19e285b
xSternLeVegetal <quentin.le.masson@outlook.com>
2024-10-27 19:21:28 +0100

    hjjhjk

---
+++ Assets/Resources/Icons/GUI/Free UI build package/Script/Slide.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Slide : MonoBehaviour {
+	Image Filler;
+	public Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		Filler = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Filler.fillAmount = slider.value;
+	}
+}
---
+++ Assets/Resources/Icons/GUI/Free UI build package/Script/Togglescript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Togglescript : MonoBehaviour {
+
+    Toggle toggle;
+
+    private void Start()
+    {
+        toggle = GetComponent<Toggle>();
+    }
+
+    public GameObject Slider;
+
+
+    private void Update()
+    {
+        if (toggle.isOn)
+        {
+            Slider.SetActive(false);
+        }
+        else
+        {
+            Slider.SetActive(true);
+        }
+    }
+}
