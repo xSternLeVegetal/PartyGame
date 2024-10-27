60129c9728510676f512a0334cd290e971c39ebb
xSternLeVegetal <quentin.le.masson@outlook.com>
2024-10-27 18:58:16 +0100

    hjjhjk

---
+++ Assets/Imports/TreesImport/Validate/TriForge Assets/Fantasy Forest Environment/Scripts/TexturePan.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace TriForge
+{
+	public class TexturePan : MonoBehaviour
+	{
+		public float scrollSpeed = 1.0f;
+		Renderer rend;
+
+		void Start()
+		{
+			rend = GetComponent<Renderer>();
+		}
+
+		void Update()
+		{
+			float offset = Time.time * scrollSpeed;
+			rend.material.mainTextureOffset = new Vector2(0, offset);
+		}
+	}
+}
+
